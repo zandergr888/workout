4c9a5708d56bc285e7c9219fc0a377dc0d16ac1e
Wayne <bu2002.geo@yahoo.com>
2023-06-30 15:09:05 -0500

    changes 6/30/23

--- components/WorkoutList.js
+++ components/WorkoutList.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { Text, View, Pressable, StyleSheet } from 'react-native';
 import getWorkouts from '../data/workouts';
+import addWorkout from './workoutListFunctions';
+
 export default function WorkoutList({ selectedDate }) {
 
     //find the workout for the selected date
-    
+
     //return the date * 2 in text
     selectedDate = selectedDate.toDateString().substring(4)
 
@@ -12,37 +14,62 @@ export default function WorkoutList({ selectedDate }) {
     const workoutsForDate = getWorkouts.filter(
         (workout) => workout.date === selectedDate
     );
-
-    return workoutsForDate.length > 0 ? (
-        workoutsForDate.map((workout, index) => (
-            <Pressable
-                key={index}
-                onPress={() => setSelectedWorkout(workout)}
-                style={styles.workout}
-            >
-                <Text style={styles.workoutName}>{workout.name}</Text>
-                <Text style={styles.bestSet}>Best Set: {workout.bestSet}</Text>
+    return (
+        <View>
+            {workoutsForDate.length > 0 ? (
+                workoutsForDate.map((workout, index) => (
+                    <Pressable
+                        key={index}
+                        onPress={() => setSelectedWorkout(workout)}
+                        style={styles.workout}
+                    >
+                        <Text style={styles.workoutName}>{workout.name}</Text>
+                        <Text style={styles.bestSet}>Best Set: {workout.bestSet}</Text>
+                    </Pressable>
+                ))
+            ) : (
+                <Text style={styles.noWorkout}>No workouts for this date</Text>
+            )}
+            <Pressable style={{
+                backgroundColor: '#37FD12',
+                padding: 20,
+                marginVertical: 2,
+                borderRadius: 12
+            }}
+                onPress={() => addWorkout()}
+            >   
+                <Text style={styles.addWorkout}>Add Workout</Text>
             </Pressable>
-            
-        ))
-    ) : (
-        <Text style={styles.noWorkout}>No workouts for this date</Text>
-    );
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
     workout: {
-        backgroundColor: 'white',
+        backgroundColor: '#333333',
         padding: 20,
-        marginVertical: 10,
+        marginVertical: 2,
+        borderRadius: 12
+    },
+    addWorkout: {
+        backgroundColor: '#37FD12',
+        color: 'black',
+        borderRadius: 23,
     },
     workoutName: {
         fontWeight: 'bold',
+        color: '#FFFFFF',
         fontSize: 18,
     },
     noWorkout: {
         color: 'white',
         marginTop: 20,
         textAlign: 'center',
+        marginBottom: 20,
+    },
+    bestSet: {
+        color: 'white',
+
+        marginTop: 10,
     },
 });
